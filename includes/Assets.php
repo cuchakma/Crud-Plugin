0ba66eb84f971dd58276ae36584e85e2f3fe2bfe
Cupid Chakma <greendaycu20@gmail.com>
2021-04-14 14:03:41 +0600

    enqueue script pattern changed

--- includes/Assets.php
+++ includes/Assets.php
@@ -11,8 +11,39 @@ class Assets {
 		add_action( 'wp_enqueue_scripts', array( $this, 'equeue_assets' ), 10, 1 );
 	}
 
+	public function get_scripts() {
+		return array(
+			'cc-scripts' => array(
+				'src'     => CC_CRUD_ASSETS . '/js/frontend.js',
+				'version' => fileatime( CC_CRUD_PATH . '/assets/js/frontend.js' ),
+				'deps'    => array( 'jQuery' ),
+			),
+		);
+	}
+
+	public function get_styles() {
+		return array(
+			'cc-styles' => array(
+				'src'     => CC_CRUD_ASSETS . '/css/frontend.css',
+				'version' => fileatime( CC_CRUD_PATH . '/assets/css/frontend.css' ),
+			),
+		);
+	}
+
 	public function equeue_assets() {
-		wp_enqueue_script( 'cc-scripts', CC_CRUD_ASSETS . '/js/frontend.js', false, fileatime( CC_CRUD_PATH . '/assets/js/frontend.js' ), true );
-		wp_enqueue_style( 'cc-styles', CC_CRUD_ASSETS . '/css/frontend.css', false, fileatime( CC_CRUD_PATH . '/assets/css/frontend.css' ) );
+
+		$scripts = $this->get_scripts();
+
+		foreach ( $scripts as $handle => $script ) {
+			$deps = isset( $script['deps'] ) ? $script['deps'] : false;
+			wp_register_script( $handle, $script['src'], $deps, $script['version'], true );
+		}
+
+		$styles = $this->get_styles();
+
+		foreach ( $styles as $handle => $style ) {
+			$deps = isset( $style['deps'] ) ? $style['deps'] : false;
+			wp_register_style( $handle, $style['src'], $deps, $style['version'] );
+		}
 	}
 }
--- includes/Frontend/Shortcode.php
+++ includes/Frontend/Shortcode.php
@@ -5,23 +5,25 @@ namespace CC\CRUD\Frontend;
 /**
  * Shortcode Handler Class
  */
-class Shortcode{
-    
-    /**
-     * Initializes the class
-     */
-    function __construct() {
-        add_shortcode( 'cc-curd', array( $this, 'render_shortcode')  );
-    }
+class Shortcode {
 
-    /**
-     * shortcode handler class
-     *
-     * @param array $atts
-     * @param string $content
-     * @return string
-     */
-    public function render_shortcode( $atts, $content = '') {
-        return "Hello From Shortcode";
-    }
+	/**
+	 * Initializes the class
+	 */
+	public function __construct() {
+		add_shortcode( 'cc-curd', array( $this, 'render_shortcode' ) );
+	}
+
+	/**
+	 * Shortcode handler class
+	 *
+	 * @param array  $atts
+	 * @param string $content
+	 * @return string
+	 */
+	public function render_shortcode( $atts, $content = '' ) {
+		wp_enqueue_script( 'cc-scripts' );
+		wp_enqueue_style( 'cc-styles' );
+		return '<div class="cc-shortcodes">Hello From Shortcode</div>';
+	}
 }
